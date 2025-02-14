dc9503839c8ac0d06849c1eb91856b22fb191320
Sami Hamaizi <sami.hamaizi@niftables.com>
2025-02-14 17:28:29 +0100

    fix: adjust for ios and safari

--- src/app/components/sections/Hero/index.tsx
+++ src/app/components/sections/Hero/index.tsx
@@ -7,78 +7,104 @@ import { X } from './icons/X'
 import { RightArrow } from './icons/RightArrow'
 import Link from 'next/link'
 import { config } from '@/config'
+import { isIOS, isSafari } from 'react-device-detect'
 
 const arrows = Array(100).fill(null)
 
 export const Hero = () => {
+  const showVideoWithEffects = isIOS || isSafari
   return (
     <div className="relative min-h-screen w-full overflow-hidden">
-      {/* Animated Background */}
-      <div className="absolute inset-0 z-20 flex">
-        <div className="animate-slide relative h-full min-w-full flex-shrink-0">
-          <Image
-            src="/svgs/squares-animated.svg"
-            alt="Background"
-            fill
-            className="object-cover"
-          />
+      {!showVideoWithEffects && (
+        <>
+          {/* Animated Background */}
+          <div className="absolute inset-0 z-20 flex">
+            <div className="animate-slide relative h-full min-w-full flex-shrink-0">
+              <Image
+                src="/svgs/squares-animated.svg"
+                alt="Background"
+                fill
+                className="object-cover"
+              />
+            </div>
+            <div className="animate-slide relative h-full min-w-full flex-shrink-0">
+              <Image
+                src="/svgs/squares-animated.svg"
+                alt="Background"
+                fill
+                className="object-cover"
+              />
+            </div>
+          </div>
+          {/* Left and Right Gradients */}
+          <div className="absolute inset-0 z-30">
+            <Image
+              src="/svgs/left-gradient.svg"
+              alt="left gradient"
+              fill
+              className="object-cover"
+            />
+          </div>
+          <div className="absolute inset-0 z-30">
+            <Image
+              src="/svgs/right-gradient.svg"
+              alt="Right gradient"
+              fill
+              className="object-cover"
+            />
+          </div>
+          <div className="absolute inset-0 z-30">
+            <Image
+              src="/images/noise.png"
+              alt="Right gradient"
+              fill
+              className="object-cover opacity-20 mix-blend-darken"
+            />
+          </div>
+          {/* Video Overlay */}
+          <div className="absolute inset-0 z-30 opacity-20">
+            <video
+              className="h-full w-full object-cover mix-blend-color-dodge"
+              autoPlay
+              loop
+              muted
+              playsInline
+              webkit-playsinline
+            >
+              <source
+                src="/videos/animation-numbers.mp4"
+                type="video/mp4"
+              />
+              Your browser does not support the video tag.
+            </video>
+          </div>
+        </>
+      )}
+      {showVideoWithEffects && (
+        <div className="absolute inset-0 z-20">
+          <video
+            className="h-full w-full object-cover"
+            autoPlay
+            loop
+            muted
+            playsInline
+            webkit-playsinline
+          >
+            <source
+              p-14
+              src="/videos/animation-number-full.mp4"
+              type="video/mp4"
+            />
+            Your browser does not support the video tag.
+          </video>
         </div>
-        <div className="animate-slide relative h-full min-w-full flex-shrink-0">
-          <Image
-            src="/svgs/squares-animated.svg"
-            alt="Background"
-            fill
-            className="object-cover"
-          />
-        </div>
-      </div>
-      {/* Left and Right Gradients */}
-      <div className="absolute inset-0 z-30">
-        <Image
-          src="/svgs/left-gradient.svg"
-          alt="left gradient"
-          fill
-          className="object-cover"
-        />
-      </div>
-      <div className="absolute inset-0 z-30">
-        <Image
-          src="/svgs/right-gradient.svg"
-          alt="Right gradient"
-          fill
-          className="object-cover"
-        />
-      </div>
-      <div className="absolute inset-0 z-30">
-        <Image
-          src="/images/noise.png"
-          alt="Right gradient"
-          fill
-          className="object-cover opacity-20 mix-blend-darken"
-        />
-      </div>
-      {/* Video Overlay */}
-      <div className="absolute inset-0 z-20">
-        <video
-          className="h-full w-full object-cover opacity-30"
-          autoPlay
-          loop
-          muted
-        >
-          <source
-            p-14
-            src="/videos/animation-numbers.mp4"
-            type="video/mp4"
-          />
-          Your browser does not support the video tag.
-        </video>
-      </div>
+      )}
       {/* Bird */}
       <Image
         src={Bird}
         alt="Bird"
         className={cn(
-          'animate-float absolute left-[30%] top-1/2 z-40 h-[90%] w-[70%] -translate-y-1/2 md:left-1/2 md:-translate-x-1/2',
+          'animate-float absolute left-[25%] top-[45%] z-40 h-[90%] w-[90%] -translate-y-1/2 sm:top-1/2 md:left-1/2 md:-translate-x-1/2',
         )}
       />
       {/* Content */}
@@ -112,7 +138,7 @@ export const Hero = () => {
         <div className="relative flex h-full flex-col">
           <h2
             className={cn(
-              'mr-4 w-full max-w-[400px] translate-x-1 font-rogan font-medium ~xs/xl:~text-[1rem]/[2rem] ~xs/xl:~leading-[1.375rem]/[2.2rem]',
+              'mr-4 w-full max-w-[400px] translate-x-1 font-rogan font-medium text-white ~xs/xl:~text-[1rem]/[2rem] ~xs/xl:~leading-[1.375rem]/[2.2rem]',
               'absolute left-[40%] top-[15%] md:left-[60%] md:top-[5%]',
             )}
           >
@@ -122,7 +148,7 @@ export const Hero = () => {
             <br /> since 2017.
           </h2>
           <div className="absolute top-1/2 w-full">
-            <h1 className="font-rogan font-extrabold uppercase ~xs/xl:~text-[3.05rem]/[8rem] ~xs/xl:~leading-[3.05rem]/[8rem]">
+            <h1 className="font-rogan font-extrabold uppercase text-white ~xs/xl:~text-[3.05rem]/[8rem] ~xs/xl:~leading-[3.05rem]/[8rem]">
               <span className="relative mr-2 text-green sm:mr-4">
                 <Image
                   src="/gifs/glow.gif"
@@ -134,10 +160,10 @@ export const Hero = () => {
               </span>
               Intel
             </h1>
-            <h2 className="font-rogan font-bold uppercase ~xs/xl:~text-[1.875rem]/[3.875rem]">
+            <h2 className="font-rogan font-bold uppercase text-white ~xs/xl:~text-[1.875rem]/[3.875rem]">
               elite trading <span className="hidden sm:inline">group</span>
             </h2>
-            <h2 className="-mt-3 font-rogan font-bold uppercase ~xs/xl:~text-[1.875rem]/[3.875rem] sm:hidden">
+            <h2 className="-mt-3 font-rogan font-bold uppercase text-white ~xs/xl:~text-[1.875rem]/[3.875rem] sm:hidden">
               group
             </h2>
             <button className="relative mt-10 hidden items-center rounded-md bg-black px-2 py-4 font-rogan text-[2rem] font-bold uppercase text-green outline-none focus:outline-none sm:flex">
@@ -151,17 +177,17 @@ export const Hero = () => {
               </span>
               <span className="pl-40 pr-24">Join now</span>
             </button>
+            <button className="mx-auto mt-16 flex w-full items-center rounded-md border-4 border-main-black bg-green px-4 py-2.5 sm:hidden">
+              <span className="flex-1 font-rogan text-2xl font-bold uppercase text-main-black">
+                Join now
+              </span>
+              <RightArrow className="h-[18px] w-[18px]" />
+            </button>
           </div>
           <p className="absolute bottom-[10%] right-[2%] hidden font-rogan text-lg font-medium leading-[1.125rem] sm:block">
             More details are coming soon
           </p>
         </div>
-        <button className="absolute bottom-4 mt-16 flex w-[calc(100%-2rem)] items-center rounded-md border-4 border-main-black bg-green px-4 py-2.5 sm:hidden">
-          <span className="flex-1 font-rogan text-2xl font-bold uppercase text-main-black">
-            Join now
-          </span>
-          <RightArrow className="h-[18px] w-[18px]" />
-        </button>
       </div>{' '}
     </div>
   )
