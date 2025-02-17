4cd61319b61434597ba9930a63708078f6099bc8
Sami Hamaizi <sami.hamaizi@niftables.com>
2025-02-17 10:54:19 +0100

    fix: ui fixes and adds favicons

--- src/app/components/sections/Hero/index.tsx
+++ src/app/components/sections/Hero/index.tsx
@@ -1,21 +1,39 @@
 'use client'
+import { useEffect } from 'react'
 import Image from 'next/image'
 import Bird from '@/../public/svgs/bird.svg'
 import { cn } from '@/lib/utils'
 import Logo from '@/../public/svgs/logo.svg'
-import { Discord } from './icons/Discord'
-import { X } from './icons/X'
-import { RightArrow } from './icons/RightArrow'
+import { Discord } from '../../icons/Discord'
+import { X } from '../../icons/X'
+import { RightArrow } from '../../icons/RightArrow'
 import Link from 'next/link'
 import { config } from '@/config'
 import { isIOS, isSafari } from 'react-device-detect'
 
 const arrows = Array(100).fill(null)
+const showVideoWithEffects = isIOS || isSafari
 
 export const Hero = () => {
-  const showVideoWithEffects = isIOS || isSafari
+  useEffect(() => {
+    // Update CSS variable on mount and resize
+    const setVH = () => {
+      document.documentElement.style.setProperty(
+        '--vh',
+        `${window.innerHeight * 0.01}px`,
+      )
+    }
+
+    setVH()
+    window.addEventListener('resize', setVH)
+    return () => window.removeEventListener('resize', setVH)
+  }, [])
+
   return (
-    <div className="relative min-h-screen w-full overflow-hidden">
+    <div
+      className="relative w-full overflow-hidden"
+      style={{ height: 'calc(var(--vh, 1vh) * 100)' }}
+    >
       {!showVideoWithEffects && (
         <>
           {/* Animated Background */}
@@ -38,7 +56,7 @@ export const Hero = () => {
             </div>
           </div>
           {/* Left and Right Gradients */}
-          <div className="absolute inset-0 z-30">
+          <div className="absolute inset-0 z-30 hidden sm:block">
             <Image
               src="/svgs/left-gradient.svg"
               alt="left gradient"
@@ -46,7 +64,15 @@ export const Hero = () => {
               className="object-cover"
             />
           </div>
-          <div className="absolute inset-0 z-30">
+          <div className="absolute inset-0 z-30 sm:hidden">
+            <Image
+              src="/svgs/left-gradient-mobile.svg"
+              alt="left gradient"
+              fill
+              className="object-cover"
+            />
+          </div>
+          <div className="absolute inset-0 z-30 hidden sm:block">
             <Image
               src="/svgs/right-gradient.svg"
               alt="Right gradient"
@@ -54,12 +80,21 @@ export const Hero = () => {
               className="object-cover"
             />
           </div>
+          <div className="absolute inset-0 z-30 sm:hidden">
+            <Image
+              src="/svgs/right-gradient-mobile.svg"
+              alt="Right gradient"
+              fill
+              className="object-cover"
+            />
+          </div>
+          {/* Noise*/}
           <div className="absolute inset-0 z-30">
             <Image
               src="/images/noise.png"
               alt="Right gradient"
               fill
-              className="object-cover opacity-20 mix-blend-darken"
+              className="hidden object-cover opacity-20 sm:inline"
             />
           </div>
           {/* Video Overlay */}
@@ -92,7 +127,6 @@ export const Hero = () => {
             webkit-playsinline
           >
             <source
-              p-14
               src="/videos/animation-number-full.mp4"
               type="video/mp4"
             />
@@ -109,7 +143,7 @@ export const Hero = () => {
         )}
       />
       {/* Content */}
-      <div className="absolute inset-0 z-50 h-full w-full px-4 py-3 sm:px-12 sm:pt-8">
+      <div className="absolute inset-0 z-50 h-full w-full p-4 sm:px-12 sm:pt-8">
         {/* Header */}
         <div className="flex w-full items-center justify-between">
           <Image
@@ -122,7 +156,7 @@ export const Hero = () => {
               href={config.socialMedia.x}
               target="_blank"
             >
-              <button className="border-light-line bg-dark-gray group rounded-md border p-3 transition-all duration-500 ease-in-out hover:bg-green">
+              <button className="group rounded-md border border-light-line bg-dark-gray p-3 transition-all duration-500 ease-in-out hover:bg-green">
                 <X className="h-4 w-4 text-white transition-colors duration-300 ease-in-out group-hover:text-black sm:h-5 sm:w-5" />
               </button>
             </Link>
@@ -130,7 +164,7 @@ export const Hero = () => {
               href={config.socialMedia.telegram}
               target="_blank"
             >
-              <button className="border-light-line bg-dark-gray group rounded-md border p-3 transition-all duration-500 ease-in-out hover:bg-green">
+              <button className="group rounded-md border border-light-line bg-dark-gray p-3 transition-all duration-500 ease-in-out hover:bg-green">
                 <Discord className="h-4 w-4 text-white transition-colors duration-300 ease-in-out group-hover:text-black sm:h-5 sm:w-5" />
               </button>
             </Link>
@@ -139,7 +173,7 @@ export const Hero = () => {
         <div className="relative flex h-full flex-col">
           <h2
             className={cn(
-              'font-rogan ~xs/xl:~text-[1rem]/[2rem] ~xs/xl:~leading-[1.375rem]/[2.2rem] mr-4 w-full max-w-[400px] translate-x-1 font-medium text-white',
+              'mr-4 w-full max-w-[400px] translate-x-1 font-rogan font-medium text-white ~xs/xl:~text-[1rem]/[2rem] ~xs/xl:~leading-[1.375rem]/[2.2rem]',
               'absolute left-[40%] top-[15%] md:left-[60%] md:top-[5%]',
             )}
           >
@@ -148,8 +182,10 @@ export const Hero = () => {
             <br /> & strategic market research
             <br /> since 2017.
           </h2>
-          <div className="absolute top-1/2 w-full">
-            <h1 className="font-rogan ~xs/xl:~text-[3.05rem]/[8rem] ~xs/xl:~leading-[3.05rem]/[8rem] font-extrabold uppercase text-white">
+        </div>
+        <div className="absolute left-0 top-0 flex h-full w-full flex-col justify-end p-4 sm:px-12 sm:py-8">
+          <div>
+            <h1 className="font-rogan font-extrabold uppercase text-white ~xs/xl:~text-[3.05rem]/[8rem] ~xs/xl:~leading-[3.05rem]/[8rem]">
               <span className="relative mr-2 text-green sm:mr-4">
                 <Image
                   src="/gifs/glow.gif"
@@ -161,13 +197,13 @@ export const Hero = () => {
               </span>
               Intel
             </h1>
-            <h2 className="font-rogan ~xs/xl:~text-[1.875rem]/[3.875rem] font-bold uppercase text-white">
+            <h2 className="font-rogan font-bold uppercase text-white ~xs/xl:~text-[1.875rem]/[3.875rem]">
               elite trading <span className="hidden sm:inline">group</span>
             </h2>
-            <h2 className="font-rogan ~xs/xl:~text-[1.875rem]/[3.875rem] -mt-3 font-bold uppercase text-white sm:hidden">
+            <h2 className="-mt-3 font-rogan font-bold uppercase text-white ~xs/xl:~text-[1.875rem]/[3.875rem] sm:hidden">
               group
             </h2>
-            <button className="font-rogan relative mt-10 hidden items-center rounded-md bg-black px-2 py-4 text-[2rem] font-bold uppercase text-green outline-none focus:outline-none sm:flex">
+            <button className="relative mt-10 hidden items-center rounded-md bg-black px-2 py-4 font-rogan text-[2rem] font-bold uppercase text-green outline-none focus:outline-none sm:flex">
               <span className="group absolute flex h-[65px] w-[65px] items-center justify-center overflow-hidden rounded-md bg-green transition-all duration-700 hover:w-[calc(100%-1rem)]">
                 <div className="animate-arrows hidden gap-5 group-hover:flex">
                   {arrows.map((_, index) => (
@@ -178,18 +214,18 @@ export const Hero = () => {
               </span>
               <span className="pl-40 pr-24">Join now</span>
             </button>
-            <button className="border-main-black mx-auto mt-16 flex w-full items-center rounded-md border-4 bg-green px-4 py-2.5 sm:hidden">
-              <span className="font-rogan text-main-black flex-1 text-2xl font-bold uppercase">
+            <button className="mx-auto mt-[40%] flex w-full items-center rounded-md border-4 border-main-black bg-green px-4 py-2.5 sm:hidden">
+              <span className="flex-1 font-rogan text-2xl font-bold uppercase text-main-black">
                 Join now
               </span>
               <RightArrow className="h-[18px] w-[18px]" />
             </button>
+            <p className="mt-4 hidden text-end font-rogan text-lg font-medium leading-[1.125rem] sm:block">
+              More details are coming soon
+            </p>
           </div>
-          <p className="font-rogan absolute bottom-[10%] right-[2%] hidden text-lg font-medium leading-[1.125rem] sm:block">
-            More details are coming soon
-          </p>
         </div>
-      </div>{' '}
+      </div>
     </div>
   )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,29 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicons/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicons/favicon-16x16.png"
+        />
+        <link
+          rel="manifest"
+          href="/favicons/site.webmanifest"
+        />
+      </head>
       <body className={`${rogan.variable}`}>{children}</body>
     </html>
   )
